919a613cd9ca827f732f2a9a798843fea6c946de
mmedhat <mmedhat@eg.ibm.com>
2022-10-23 16:53:40 +0200

    Hello From Local Webide

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("ListReportFioriElemets.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("ListReportFioriElemets.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
---
+++ webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,9 @@
+sap.ui.controller("ListReportFioriElemets.ext.controller.ListReportExt", {
+
+	onListNavigationExtension: function(oEvent) {
+	 alert('msg');
+	},
+	        onBeforeRebindTableExtension: function(oEvent) {
+                alert('Yarab');
+            }
+});
